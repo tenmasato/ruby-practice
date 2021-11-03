daffd2ac0a1d797f34e403b3f53bc795b224f31c
tenmasato <narito1015@icloud.com>
2021-11-03 05:26:18 +0000

    [Finish]

---
+++ lesson6/new_method1.rb
@@ -0,0 +1,7 @@
+#baseは下辺、heightは高さを表す変数
+
+def calc_triangle_area(base,height)
+  base * height / 2
+end
+
+p calc_triangle_area(10,5)
---
+++ lesson6/new_method2.rb
@@ -0,0 +1,8 @@
+def calc_triangle_area(base,height)
+  base * height / 2
+end
+
+mybase = 12
+myheight = 3
+
+p calc_triangle_area(mybase,myheight)
